4337377377f7f676077c3f2139e31736b2d7f12e
Work <Work@MSI>
2023-11-01 11:03:57 -0500

    Where we ended on Oct 31 (last day spent on linked lists)

--- COSC2436-H01-Topic4/Source.cpp
+++ COSC2436-H01-Topic4/Source.cpp
@@ -1,42 +1,175 @@
 #include <iostream>
 
+#include<queue>
+
 using namespace std;
 
+template<typename T> 
 struct Node
 {
-    int data; 
+    T info; 
 
-    Node* link; 
-
-    //Node()
-    //{
-
-    //}
+    Node<T>* link; 
 };
+
+template<typename T>
+class LinkedList
+{
+
+private:
+    int nodeCount; //not mandatory, but convenient
+    Node<T>* first;
+    Node<T>* last;
+
+public: 
+    LinkedList()
+    {
+        nodeCount = 0;
+        first = NULL; //or nullptr keyword, or 0
+        last = NULL;
+    }
+
+    void print() {
+
+        cout << "List contents: " << endl; 
+
+        Node<T>* current;
+        current = first;
+        while (current != NULL) {
+            cout << current->info << " ";
+            current = current->link;
+        }
+
+        cout << "\n";
+    }//end print() function
+
+    int length() { return nodeCount; }
+
+    T getLastNodeContents()
+    {
+        return last->info; 
+    }
+
+    void insertFirst(const T& newInfo)
+    {
+        Node<T>* newNode = new Node<T>;
+        newNode->info = newInfo;
+
+        newNode->link = first;
+
+        first = newNode; 
+
+        if (last == NULL)
+        {
+            last = newNode;
+            //if newNode is the only node,
+            //it is both first and last
+        }
+
+        nodeCount++; 
+    }
+    //searchForNodeWithValue()
+
+    void insertAfter(Node<T>* previous, T insertedValue)
+    {
+        Node<T>* newNode = new Node<T>;
+        newNode->info = insertedValue;
+
+        //Diagram on next slides walks through two lines below:
+        newNode->link = previous->link;
+        previous->link = newNode;
+
+        nodeCount++; 
+    }
+
+
+    //insertAfterGivenNode(Node, int valueToInsert)
+    Node<T>* getNode(int nodePosition)
+    {
+        Node<T>* current = new Node<T>;
+        current = first;
+        int i = 0;
+        while (i < nodePosition)
+        {
+            current = current->link;
+            i++;
+        }
+        return current;
+    }
+
+    bool searchForNodeWithValue(T targetValue)
+    {
+        Node<T>* current = new Node<T>; 
+
+        current = first; 
+
+        bool isFound = false; 
+        
+
+        int nodeCounter = 0; 
+        while (current != NULL && !isFound)
+        {
+            if (current->info == targetValue)
+            {
+                isFound = true; 
+            }
+
+            else
+            {
+                current = current->link;
+                nodeCounter++; 
+            }
+        } //end while
+
+        cout << "Node counter final value is: " << nodeCounter << endl; 
+        return isFound; 
+    } //end `search`
+};
+
 
 int main()
 {
+    LinkedList<char> charList; 
+    charList.insertFirst('a');
 
-    Node* firstNode = new Node; 
-    firstNode->data = 234; 
-    firstNode->link = nullptr;
+    charList.insertFirst('c'); //c a 
 
-    Node* secondNode = new Node; 
-    secondNode->data = 126; 
-    secondNode->link = nullptr; 
+    Node<char>* previousNode = charList.getNode(0);  //Node containing 'c'
 
-    //UPDATE the address of first node's link: (so that it points to secondNode)
-    firstNode->link = secondNode; 
+    charList.insertAfter(previousNode, 'b');
 
+    Node<char>* ANOTHERpreviousNode = charList.getNode(1);  //Node containing 'b'
 
-    Node* thirdNode = new Node; 
-    thirdNode->data = -256; 
-    thirdNode->link = nullptr; 
-
-    secondNode->link = thirdNode; 
+    charList.insertAfter(ANOTHERpreviousNode, '9');
 
 
-    cout << firstNode->link->link->data << endl; 
+    charList.print();  //c b 9 a?
+    cout << "length: " << charList.length() << endl; //4? 
+
+    cout << "\n\n" << "Is the value 'b' in the list? " << endl; 
+    cout << std::boolalpha << charList.searchForNodeWithValue('b') << endl; 
+
+    //cout << "LAST element is: " << charList.getLastNodeContents() << endl; 
+    //Node* firstNode = new Node; 
+    //firstNode->data = 234; 
+    //firstNode->link = nullptr;
+
+    //Node* secondNode = new Node; 
+    //secondNode->data = 126; 
+    //secondNode->link = nullptr; 
+
+    ////UPDATE the address of first node's link: (so that it points to secondNode)
+    //firstNode->link = secondNode; 
+
+
+    //Node* thirdNode = new Node; 
+    //thirdNode->data = -256; 
+    //thirdNode->link = nullptr; 
+
+    //secondNode->link = thirdNode; 
+
+
+    //cout << firstNode->link->link->data << endl; 
 
     ////cout << firstNode.data << endl; 
     ////cout << firstNode.link << endl; 
